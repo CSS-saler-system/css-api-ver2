161824e4c9ae3986fade331dad8bdeb710e3a991
sumo6842 <Linhduc13gb>
2022-05-29 09:43:46 +0700

    change little in repository

--- src/main/java/com/springframework/csscapstone/data/repositories/AccountRepository.java
+++ src/main/java/com/springframework/csscapstone/data/repositories/AccountRepository.java
@@ -13,20 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Transactional(readOnly = true)
 public interface AccountRepository extends JpaRepository<Account, UUID>, JpaSpecificationExecutor<Account> {
 //    Optional<Account> findAccountByUsername(String username);
-    @Transactional(readOnly = true)
+
     Optional<Account> findAccountByEmail(String email);
 
-    @Transactional(readOnly = true)
     Optional<Account> findAccountsByPhone(String phone);
 
-    @Transactional(readOnly = true)
     Optional<List<Account>> findAccountByEmailOrPhone(String email, String phone);
 
-
-    @Transactional(readOnly = true)
     @Query(value = "SELECT a " +
             "FROM Account a " +
             "LEFT JOIN FETCH a.images " +
@@ -34,8 +30,7 @@ public interface AccountRepository extends JpaRepository<Account, UUID>, JpaSpec
     countQuery = "SELECT a FROM Account a WHERE a.role.name =: role")
     Page<Account> findAccountByRole(@Param("role") String role, Pageable pageable);
 
-    @Transactional(readOnly = true)
-    @Query("SELECT a FROM Account a LEFT JOIN FETCH a.role")
-    List<Account> findAllFetchJoinRole();
+    @Query("SELECT a FROM Account a WHERE a.role.name =: role")
+    List<Account> findAllFetchJoinRole(@Param("role") String roleName);
 
 }
