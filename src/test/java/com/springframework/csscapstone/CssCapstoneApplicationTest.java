a0f917945585d61ce4c42ea57f4a890ada012452
aumo6842 <sumo6842@gmail.com>
2022-06-03 11:30:19 +0700

    git ignore

--- src/test/java/com/springframework/csscapstone/CssCapstoneApplicationTest.java
+++ src/test/java/com/springframework/csscapstone/CssCapstoneApplicationTest.java
@@ -1,4 +1,12 @@
 package com.springframework.csscapstone;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CssCapstoneApplicationTest {
+    @Test
+    void getAllProductTests() {
+
+    }
 }
