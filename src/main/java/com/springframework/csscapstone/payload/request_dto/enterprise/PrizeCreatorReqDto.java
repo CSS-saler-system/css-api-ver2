1e57424f35fe7078fcdfb0d26cb3fb85263651a8
sumo6842 <Linhduc13gb>
2022-06-09 11:06:02 +0700

    prepare create and update prize

---
+++ src/main/java/com/springframework/csscapstone/payload/request_dto/enterprise/PrizeCreatorReqDto.java
@@ -0,0 +1,42 @@
+package com.springframework.csscapstone.payload.request_dto.enterprise;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
+
+@Data
+public class PrizeCreatorReqDto {
+    private final String name;
+    private final Double price;
+    private final Long quantity;
+    private final String description;
+    private final AccountDto creator;
+
+    public PrizeCreatorReqDto(
+            @JsonProperty("name") String name,
+            @JsonProperty("price") Double price,
+            @JsonProperty("quantity") Long quantity,
+            @JsonProperty("description") String description,
+            @JsonProperty("creator") AccountDto creator) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+        this.creator = creator;
+    }
+
+    @Data
+    public static class AccountDto implements Serializable {
+        private final UUID id;
+
+        @JsonCreator(mode = PROPERTIES)
+        public AccountDto(UUID id) {
+            this.id = id;
+        }
+    }
+}
--- src/main/java/com/springframework/csscapstone/services/PrizeService.java
+++ src/main/java/com/springframework/csscapstone/services/PrizeService.java
@@ -1,10 +1,19 @@
 package com.springframework.csscapstone.services;
 
+import com.springframework.csscapstone.payload.request_dto.enterprise.PrizeCreatorReqDto;
+import com.springframework.csscapstone.payload.request_dto.enterprise.PrizeUpdaterReqDto;
 import com.springframework.csscapstone.payload.response_dto.PageImplResDto;
 import com.springframework.csscapstone.payload.response_dto.enterprise.PrizeResDto;
+
+import java.util.UUID;
 
 public interface PrizeService {
 
     PageImplResDto<PrizeResDto> getAll(String name, Integer pageNumber, Integer pageSize);
     PageImplResDto<PrizeResDto> getAll(Integer pageNumber, Integer pageSize);
+
+    UUID updatePrize(PrizeUpdaterReqDto prizeUpdater);
+
+    UUID createPrize(PrizeCreatorReqDto prizeCreatorReqDto);
+
 }
