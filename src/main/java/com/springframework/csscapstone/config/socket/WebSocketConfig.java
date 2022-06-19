ce850f9680c7845ed64f39de58bfda733f0c96ec
aumo6842 <sumo6842@gmail.com>
2022-06-19 23:10:05 +0700

    clear tests properties

--- src/main/java/com/springframework/csscapstone/config/socket/WebSocketConfig.java
+++ src/main/java/com/springframework/csscapstone/config/socket/WebSocketConfig.java
@@ -6,20 +6,20 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//@Configuration
-//@EnableWebSocketMessageBroker
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/topic");
-//        registry.setApplicationDestinationPrefixes("/app");
-//
-//    }
-//
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/ws-message")
-//                .setAllowedOriginPatterns("*")
-//                .withSockJS();
-//    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-message")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
 }
--- src/main/java/com/springframework/csscapstone/controller/WebsocketController.java
+++ src/main/java/com/springframework/csscapstone/controller/WebsocketController.java
@@ -14,26 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
-//@Tag(name = "Sharing")
-//@RequestMapping("user")
-//@RestController
-//@RequiredArgsConstructor
+@Tag(name = "Sharing")
+@RequestMapping("user")
+@RestController
+@RequiredArgsConstructor
 public class WebsocketController {
-//    private final SimpMessagingTemplate template;
-//
-//    @PostMapping("/send")
-//    public ResponseEntity<?> sendMessage(@RequestBody String textMessageDto) {
-//        template.convertAndSend("/topic/message", textMessageDto);
-//        return new ResponseEntity<>(OK);
-//    }
-//
-//    @MessageMapping("/sendMessage")
-//    public void receiveMessage(@Payload String textMessageDto) {
+    private final SimpMessagingTemplate template;
+
+    @PostMapping("/send")
+    public ResponseEntity<?> sendMessage(@RequestBody String textMessageDto) {
+        template.convertAndSend("/topic/message", textMessageDto);
+        return new ResponseEntity<>(OK);
+    }
+
+    @MessageMapping("/sendMessage")
+    public void receiveMessage(@Payload String textMessageDto) {
 //        receive message from client
-//    }
-//
-//    @SendTo("/topic/message")
-//    public String broadcastMessage(@Payload String dto) {
-//        return dto;
-//    }
+    }
+
+    @SendTo("/topic/message")
+    public String broadcastMessage(@Payload String dto) {
+        return dto;
+    }
 }
