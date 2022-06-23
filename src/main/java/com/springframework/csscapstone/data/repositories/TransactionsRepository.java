b617bafaac30cd0268aa39a370dd77f69c3771af
sumo6842 <Linhduc13gb>
2022-06-23 21:02:19 +0700

    transaction dto

--- src/main/java/com/springframework/csscapstone/data/repositories/TransactionsRepository.java
+++ src/main/java/com/springframework/csscapstone/data/repositories/TransactionsRepository.java
@@ -4,6 +4,7 @@ import com.springframework.csscapstone.data.domain.Transactions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -13,10 +14,10 @@ import java.util.UUID;
 
 @Transactional(readOnly = true)
 public interface TransactionsRepository
-        extends PagingAndSortingRepository<Transactions, UUID>, JpaSpecificationExecutor<Transactions> {
+        extends JpaRepository<Transactions, UUID>, JpaSpecificationExecutor<Transactions> {
 
-    @Query(value = "SELECT t FROM Transactions t " +
-            "WHERE NOT EXISTS (SELECT T1 FROM Transactions T1 WHERE T1.transactionStatus = 'DISABLED')")
-    Page<Transactions> findAll(Pageable pageable);
+//    @Query(value = "SELECT t FROM Transactions t " +
+//            "WHERE NOT EXISTS (SELECT T1 FROM Transactions T1 WHERE T1.transactionStatus = 'DISABLED')")
+//    Page<Transactions> findAll(Pageable pageable);
 
 }
--- src/main/java/com/springframework/csscapstone/payload/response_dto/enterprise/TransactionsResDto.java
+++ src/main/java/com/springframework/csscapstone/payload/response_dto/enterprise/TransactionsResDto.java
@@ -1,12 +1,10 @@
 package com.springframework.csscapstone.payload.response_dto.enterprise;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.springframework.csscapstone.data.status.TransactionStatus;
 import com.springframework.csscapstone.payload.basic.BillImageBasicDto;
 import lombok.Data;
@@ -14,72 +12,32 @@ import lombok.Data;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
-
-import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
 
 @Data
 public class TransactionsResDto implements Serializable {
     private final UUID id;
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
-    private final LocalDateTime createTransaction;
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private final LocalDateTime createTransactionDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
-    private final LocalDateTime lastModifiedDate;
+    private final LocalDateTime LastModifiedDate;
+
     private final List<BillImageBasicDto> billImage;
     private final double point;
     private final TransactionStatus transactionStatus;
-    private final Set<AccountInnerTransactionsResDto> account;
-
-    @JsonCreator(mode = PROPERTIES)
-    public TransactionsResDto(
-            @JsonProperty("id") UUID id,
-            @JsonProperty("createTransaction") LocalDateTime createTransaction,
-            @JsonProperty("lastModifiedDate") LocalDateTime lastModifiedDate,
-            @JsonProperty("billImage") List<BillImageBasicDto> billImage,
-            @JsonProperty("point") double point,
-            @JsonProperty("transactionStatus") TransactionStatus transactionStatus,
-            @JsonProperty("account") Set<AccountInnerTransactionsResDto> account) {
-        this.id = id;
-        this.createTransaction = createTransaction;
-        this.lastModifiedDate = lastModifiedDate;
-        this.billImage = billImage;
-        this.point = point;
-        this.transactionStatus = transactionStatus;
-        this.account = account;
-    }
+    private final AccountDto transactionCreator;
+    private final AccountDto transactionApprover;
 
     @Data
-    public static class AccountInnerTransactionsResDto implements Serializable {
+    public static class AccountDto implements Serializable {
         private final UUID id;
         private final String name;
-        private final RoleInnerTransactionResDto role;
-
-        @JsonCreator(mode = PROPERTIES)
-        public AccountInnerTransactionsResDto(
-                @JsonProperty("id") UUID id,
-                @JsonProperty("name") String name,
-                @JsonProperty("role") RoleInnerTransactionResDto role) {
-            this.id = id;
-            this.name = name;
-            this.role = role;
-        }
-
-        @Data
-        public static class RoleInnerTransactionResDto implements Serializable {
-            private final String id;
-            private final String name;
-            @JsonCreator(mode = PROPERTIES)
-            public RoleInnerTransactionResDto(
-                    @JsonProperty("id") String id,
-                    @JsonProperty("name") String name) {
-                this.id = id;
-                this.name = name;
-            }
-        }
+        private final String email;
     }
 }
