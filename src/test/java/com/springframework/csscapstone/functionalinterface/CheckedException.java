0603854e0ec7d311b1fe667ff8cfd44968040a3f
Duc Tran <sumo6842@gmail.com>
2022-06-28 07:53:34 +0700

    Delete src/test/java/com/springframework/csscapstone/functionalinterface directory

--- src/test/java/com/springframework/csscapstone/functionalinterface/CheckedException.java
+++
@@ -1,6 +0,0 @@
-package com.springframework.csscapstone.functionalinterface;
-
-@FunctionalInterface
-public interface CheckedException<T,R> {
-    R apply(T t) throws RuntimeException;
-}
--- src/test/java/com/springframework/csscapstone/functionalinterface/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.springframework.csscapstone.functionalinterface;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-@FunctionalInterface
-public interface ExceptionHandler<E, S> {
-    S jsonExceptionHandler(E e) throws JsonProcessingException;
-
-}
