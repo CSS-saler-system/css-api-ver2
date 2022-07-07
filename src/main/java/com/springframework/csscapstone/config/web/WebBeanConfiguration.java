a34d476a02703e66f1b140a0302b401d7dc61388
sumo6842 <Linhduc13gb>
2022-07-07 08:48:13 +0700

    bug compile

--- src/main/java/com/springframework/csscapstone/config/web/WebBeanConfiguration.java
+++ src/main/java/com/springframework/csscapstone/config/web/WebBeanConfiguration.java
@@ -1,11 +1,8 @@
 package com.springframework.csscapstone.config.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration
 @EnableJpaAuditing
