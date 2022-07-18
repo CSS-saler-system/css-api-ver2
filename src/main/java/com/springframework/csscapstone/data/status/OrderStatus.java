b33a0db3caa3ac99163b91f21a253d2402185fc9
meoupa1999 <57126326+meoupa1999@users.noreply.github.com>
2022-07-18 22:16:51 +0700

    Update OrderStatus.java

--- src/main/java/com/springframework/csscapstone/data/status/OrderStatus.java
+++ src/main/java/com/springframework/csscapstone/data/status/OrderStatus.java
@@ -3,8 +3,7 @@ package com.springframework.csscapstone.data.status;
 public enum OrderStatus {
     //enterprise
     FINISH,
-    DISABLE, //delete
-    CREATED,
+    DISABLE, //delete    
     //collaborator
     CANCEL,
     WAITING,//delete
