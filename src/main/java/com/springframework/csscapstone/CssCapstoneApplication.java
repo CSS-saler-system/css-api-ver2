9f1482195bb428b71a70d5cb6ea133b668ab7007
aumo6842 <sumo6842@gmail.com>
2022-08-06 20:54:28 +0700

    bug get collaborator and his performance prepare cache product list

--- src/main/java/com/springframework/csscapstone/CssCapstoneApplication.java
+++ src/main/java/com/springframework/csscapstone/CssCapstoneApplication.java
@@ -1,13 +1,9 @@
 package com.springframework.csscapstone;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableCaching
